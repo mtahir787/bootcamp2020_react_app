3ce00ff2b37d90dec1b4ea48e189b5cd8701a55a
M.TahirNazir <mtahirnazir788@gmail.com>
2020-11-25 12:40:19 -0800

    Added Dinner file with changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
-  return <h1>Hello world by tahir</h1>
+  return (
+    <div className="App">
+      <Dinner dishName="chiken karahi" sweetName="RasGullah" />
+      <hr/>
+      <Dinner dishName="chiken biryani" sweetName="Laddoo" />
+      <hr/>
+      <Dinner dishName="Mutton biryani" sweetName="Gajar ka Halwa" />
+    </div>
+  );
 }
 
 export default App;
